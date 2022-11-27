5b624c58d77894a62a0225153e8f977f0e78b53c
nataliaChrz <natiglasgow@gmail.com>
2022-11-27 14:42:21 +0000

    Fixed Bugs
    Bugs fixed in planting

--- Assets/Scripts/PlantPot.cs
+++ Assets/Scripts/PlantPot.cs
@@ -5,33 +5,38 @@ using UnityEngine;
 public class PlantPot : MonoBehaviour
 {
     public GameObject sprout;
+    private GameObject nearTo = null;
+    
 
     public void Start()
     {
         sprout.SetActive(false);
 
     }
+
+    private void Update()
+    {
+        if (nearTo != null && Input.GetKeyDown(KeyCode.P))
+        {
+             Debug.Log("Key Press");
+
+            if (Seed.seedCount >= 1)
+            {
+                sprout.SetActive(true);
+                Seed.seedCount = Seed.seedCount -= 1;
+                Debug.Log("Seed has been planted " + Seed.seedCount);
+            }
+            nearTo = null; //set to null when we're done
+        }
+    }
+
     public void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            //plantText.SetActive(true);
+            nearTo = other.gameObject;
         }
     }
 
-    public void OnTriggerStay(Collider other)
-    {
-        if (other.CompareTag("Player") && Input.GetKey(KeyCode.P))
-        {
-            if (Seed.seedCount >= 1)
-            {
-                sprout.SetActive(true);
-                Seed.seedCount =- 1;
-                Debug.Log("Seed has been planted " + Seed.seedCount);
-            }
-
-            
-
-        }
-    }
+    
 }
