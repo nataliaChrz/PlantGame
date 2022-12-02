dfd258fccefdd1d075e777cddb49b51a058f80d3
nataliaChrz <natiglasgow@gmail.com>
2022-12-02 12:16:10 +0000

    fixed menu

--- Assets/Menu.cs
+++ Assets/Menu.cs
@@ -1,16 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using unityEngine.SceneManagement;
+using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
-  public void Quitbutton() { 
-    Application.Quit();
-    Debug.Log("Game closed");
-  }
+    
+    public void Quitbutton()
+    {
+        Application.Quit();
+        Debug.Log("Game closed");
+    }
 
-  public void StartGame() {
-    SceneManager.LoadScene("PlantScene")
-  }
+    public void StartGame()
+    {
+        SceneManager.LoadScene("PlantScene");
+    }
 }
