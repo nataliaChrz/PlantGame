79c20966686155773c6bdeb5889afadc265e8c95
Heatherf3 <Heatherfletcher45@gmail.com>
2022-12-08 12:36:27 +0000

    Adding audio

---
+++ Assets/Scripts/RadioController.cs
@@ -0,0 +1,29 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadioController : MonoBehaviour
+{
+    private AudioSource radioAudioSource;
+
+    private void Start() 
+    {
+        radioAudioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlayAudio()
+    {
+        radioAudioSource.Play();
+    }
+    
+    public void PauseAudio()
+    {
+        radioAudioSource.Pause();  
+    }
+
+    public void StopAudio()
+    {
+        radioAudioSource.Stop();
+    }
+}
