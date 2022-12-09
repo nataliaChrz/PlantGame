92f05d46a8b99e8c6ceb52e265c9528d09470b50
Heatherf3 <Heatherfletcher45@gmail.com>
2022-12-09 11:34:50 +0000

    Skipping song

--- Assets/Scripts/RadioController.cs
+++ Assets/Scripts/RadioController.cs
@@ -2,14 +2,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class RadioController : MonoBehaviour
 {
+    [Header("List of Tracks")]
+    [SerializeField] private Track[] audioTracks;
+    private int trackIndex;
+
+    [Header("Text UI")]
+    [SerializeField] private Text trackTextUI;
+
     private AudioSource radioAudioSource;
 
     private void Start() 
     {
         radioAudioSource = GetComponent<AudioSource>();
+
+        trackIndex = 0;
+        radioAudioSource.clip = audioTracks[trackIndex].trackAudioClip;
+        trackTextUI.text = audioTracks[trackIndex].name;
+    }
+
+ public void SkipForwardButton()
+    {
+        if (trackIndex < audioTracks.Length - 1)
+        {
+            trackIndex++;
+            
+        }
+    }
+
+    public void SkipBackwardsButton()
+    {
+        if (trackIndex >= 1)
+        {
+            trackIndex--;
+            
+        }
+    }
+
+    void UpdateTrack(int index)
+    {
+        radioAudioSource.clip = audioTracks[index].trackAudioClip;
+        trackTextUI.text = audioTracks[index].name;
     }
     
     public void AudioVolume(float volume)
